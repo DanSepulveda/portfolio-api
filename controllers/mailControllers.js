b69ee04775038c4d8f3e9ee38fcd98e2765c1ceb
Daniel Sepúlveda <d.sepulveda.perez@gmail.com>
2022-01-07 16:12:17 -0300

    Email styles edited

--- controllers/mailControllers.js
+++ controllers/mailControllers.js
@@ -9,13 +9,26 @@ const transporter = nodemailer.createTransport({
         clientId: process.env.CLIENTID,
         clientSecret: process.env.CLIENTSECRET,
         refreshToken: process.env.REFRESHTOKEN,
-    },
-    // tls: { rejectUnauthorized: false }
+    }
 })
 
 const mailControllers = {
     sendMail: async (req, res) => {
         const { name, email, message, language } = req.body;
+        const texts = {}
+
+        if (language === 'es') {
+            texts.title = 'Mensaje Recibido!'
+            texts.greeting = 'Hola'
+            texts.message = 'Muchas gracias por contactarte conmigo. Te responderé lo antes posible. Esta es un copia de tu mensaje:'
+            texts.footer = '2022 © Todos los Derechos Reservados'
+        } else {
+            texts.title = 'Message Recieved!'
+            texts.greeting = 'Hi'
+            texts.message = 'Thank you very much for contacting me. I will answer you as soon as possible. This is a copy of your message:'
+            texts.footer = '2022 © All Rights Reserved'
+        }
+
         const options = {
             from: "Dansep <dansepdev@gmail.com>",
             to: email,
@@ -35,87 +48,12 @@ const mailControllers = {
                     margin: 0;
                     padding: 0;
                 }
-                body {
-                    background-color: #f9f9f9;
-                    width: 100%;
-                    display: flex;
-                    justify-content: center;
-                }
-                td {
-                    padding: 0 20px;
-                }
-                tr {
-                    background-color: rgb(203, 219, 239);
-                }
-                img.image {
-                    display: block;
-                    width: 128px;
-                }
                 p {
-                    font-family: Georgia, Times, 'Times New Roman', serif;
-                    color: #2f2f2f;
-                    line-height: inherit;
-                    text-align: center;
-                    font-size: 16px;
-                }
-                div.par {
-                    font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
-                    color: #2f2f2f;
-                    line-height: 1.5;
-                }
-                a {
-                    text-decoration: none;
-                    margin: 0 7px;
-                }
-                .icon {
-                    width: 32px;
-                    height: 32px;
-                }
-                .box {
-                    border: 2px solid #5d77a9;
-                    box-shadow: 1px 1px 3px rgba(47, 47, 47, 0.3), -1px -1px 3px rgba(47, 47, 47, 0.3);
-                    border-radius: 5px;
-                    width: 65%;
-                    margin: 0 auto;
-                    margin-top: 30px;
-                    padding: 15px 10px;
-                }
-                @media (max-width:700px) {
-                    .icons-inner {
-                        text-align: center;
-                    }
-            
-                    .icons-inner td {
-                        margin: 0 auto;
-                    }
-            
-                    .row-content {
-                        width: 100% !important;
-                    }
-            
-                    .stack .column {
-                        width: 100%;
-                        display: block;
-                    }
-            
-                    .reverse {
-                        display: table;
-                        width: 100%;
-                    }
-            
-                    .reverse .column.last {
-                        display: table-header-group !important;
-                    }
-            
-                    .row-2 td.column.last>table {
-                        padding-left: 0;
-                        padding-right: 0;
-                    }
+                    font-family: Georgia, Times, 'Times New Roman', serif; color: #2f2f2f; line-height: inherit; text-align: center; font-size: 16px;
                 }
             </style>
             </head>
-            
-            <body>
+            <body style="background-color: #f9f9f9; width: 100%; display: flex; justify-content: center;">
                 <table border="0" cellpadding="0" cellspacing="0" style="background-color: #f9f9f9;" width="680px">
                     <thead>
                         <tr style="background-color: #5d77a9">
@@ -123,34 +61,38 @@ const mailControllers = {
                                 <div align="center">
                                     <img src="https://i.ibb.co/dPLrcsM/logo.png" width="70" />
                                 </div>
-                                <p style="font-size: 25px; color: #ecece8; font-style: italic; margin-left: 20px;">Daniel Sepúlveda
-                                    Portfolio</p>
+                                <p style="font-size: 20px; color: #ecece8; font-style: italic; text-align: center">
+                                    Daniel Sepúlveda Portfolio
+                                </p>
                             </td>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td style="width:100%; padding-top:30px">
+                        <tr style="background-color: rgb(203, 219, 239)">
+                            <td style="width:100%; padding: 0 20px; padding-top:30px">
                                 <div align="center">
-                                    <img alt="Envelope Icon" src="https://i.ibb.co/rtyKmmk/envelope.png" class="image" />
+                                    <img alt="Envelope Icon" src="https://i.ibb.co/rtyKmmk/envelope.png" width="128" style="display: block" />
                                 </div>
                             </td>
                         </tr>
-                        <tr>
-                            <td>
-                                <p style="font-size: 35px; padding-bottom:25px;">Mensaje Recibido</p>
+                        <tr style="background-color: rgb(203, 219, 239)">
+                            <td style="padding 0 20px">
+                                <p style="font-size: 35px; padding-bottom:25px; font-family: Georgia, Times, 'Times New Roman', serif; color: #2f2f2f; line-height: inherit; text-align: center;">
+                                    ${texts.title}
+                                </p>
                             </td>
                         </tr>
-                        <tr>
-                            <td style="padding-bottom:40px; padding-top:10px;">
-                                <div class="par">
-                                    <p style="margin-bottom: 20px;">Hi <u><strong>${name}</strong></u>,</p>
-                                    <p style="width: 60%; text-align: center; margin: 0 auto;">
-                                        Muchas gracias por contactarte conmigo. Te responderé lo antes posible. Esta es un copia de
-                                        tu mensaje:
+                        <tr style="background-color: rgb(203, 219, 239)">
+                            <td style="padding: 0 20px; padding-bottom:40px; padding-top:10px;">
+                                <div style="font-family: Arial, Helvetica Neue, Helvetica, sans-serif; color: #2f2f2f; line-height: 1.5;">
+                                    <p style="margin-bottom: 20px; text-align: center; font-size: 16px; margin: 0 auto;">
+                                        ${texts.greeting} <u><strong>${name}</strong></u>,
                                     </p>
-                                    <div class="box">
-                                        <p style="width: 80%; text-align: center; margin: 0 auto;">
+                                    <p style="width: 60%; text-align: center; margin: 0 auto; font-size: 16px;">
+                                        ${texts.message}
+                                    </p>
+                                    <div style="border: 2px solid #5d77a9; box-shadow: 1px 1px 3px rgba(47, 47, 47, 0.3), -1px -1px 3px rgba(47, 47, 47, 0.3); border-radius: 5px; width: 65%; margin: 0 auto; margin-top: 30px; padding: 15px 10px;">
+                                        <p style="width: 80%; text-align: center; margin: 0 auto; font-size: 16px;">
                                             ${message}
                                         </p>
                                     </div>
@@ -160,24 +102,25 @@ const mailControllers = {
                     </tbody>
                     <tfooter>
                         <tr style="background-color: #5d77a9">
-                            <td style="text-align: center; padding-top: 30px; padding-bottom: 20px;">
-                                <a href="https://dansep.vercel.app/">
-                                    <img class="icon" src="https://i.ibb.co/KwGnRRt/webpage.png" alt="World Icon" />
+                            <td style="text-align: center; padding: 0 20px; padding-top: 30px; padding-bottom: 20px;">
+                                <a href="https://dansep.vercel.app/" style="text-decoration: none; margin-left: 7px; margin-right: 7px">
+                                    <img width="32" height="32" src="https://i.ibb.co/KwGnRRt/webpage.png" alt="World Icon" />
                                 </a>
-                                <a href="https://www.linkedin.com/in/dansepulvedap/">
-                                    <img class="icon" src="https://i.ibb.co/ZzYXczV/linkedin.png" alt="Linkedin Icon" />
+                                <a href="https://www.linkedin.com/in/dansepulvedap/" style="text-decoration: none; margin-left: 7px; margin-right: 7px">
+                                    <img width="32" height="32" src="https://i.ibb.co/ZzYXczV/linkedin.png" alt="Linkedin Icon" />
                                 </a>
-                                <a href="https://github.com/DanSepulveda">
-                                    <img class="icon" src="https://i.ibb.co/94fMN5L/github.png" alt="Github Icon" />
+                                <a href="https://github.com/DanSepulveda" style="text-decoration: none; margin-left: 7px; margin-right: 7px">
+                                    <img width="32" height="32" src="https://i.ibb.co/94fMN5L/github.png" alt="Github Icon" />
                                 </a>
                             </td>
                         </tr>
                         <tr style="background-color: #5d77a9;">
-                            <td style="padding-bottom: 30px;">
-                                <div class="par">
-                                    <p style="font-size: 13px; color: #f9f9f9;">Santiago, Chile.</p>
-                                    <p style="font-size: 13px; color: #f9f9f9;">dansepdev@gmail.com</p>
-                                    <p style="font-size: 14px; color: #cfceca; margin-top: 20px;">2021 © All Rights Reserved
+                            <td style="padding: 0 20px; padding-bottom: 30px;">
+                                <div style="font-family: Arial, Helvetica Neue, Helvetica, sans-serif; color: #2f2f2f; line-height: 1.5;">
+                                    <p style="font-size: 13px; color: #f9f9f9; font-family: Georgia, Times, 'Times New Roman', serif; text-align: center; margin: 0 auto">Santiago, Chile.</p>
+                                    <p style="font-size: 13px; color: #f9f9f9; font-family: Georgia, Times, 'Times New Roman', serif; text-align: center; margin: 0 auto">dansepdev@gmail.com</p>
+                                    <p style="font-size: 14px; color: #cfceca; margin-top: 20px; font-family: Georgia, Times, 'Times New Roman', serif; text-align: center; margin: 0 auto">
+                                        ${texts.footer}
                                     </p>
                                 </div>
                             </td>
